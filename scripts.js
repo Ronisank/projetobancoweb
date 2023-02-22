d114d44341ce09e0936d858e512807a7a85d315d
Roni <ronisank21@hotmail.com>
2023-02-22 20:54:41 -0300

    atualizando-codigo-js

--- scripts.js
+++ scripts.js
@@ -64,27 +64,51 @@ function efetuarOperacao(acao) {
             sacar(valor);
             break;
         case 'saldo':
-            alert(`Saldo atual é: R$ ${conta.saldo}`);
+            verSaldo();
             break;
         case 'depositar':
             conta.creditar(valor);
-            alert(`Depósito de R$ ${valor} efetuado com sucesso!`);
+            verDeposito();
             break;
         case 'extrato':
             consultarExtrato();
+            verExtrato();
             break;
         default:
             alert('Operação inválida!');
     }
     limparValor();
 }
+function verSaldo() {
+    const telaSaldo = document.getElementById("saida");
+    telaSaldo.innerHTML = `Saldo atual é: R$ ${conta.saldo}`;
+    telaSaldo.classList.add("saldo");
+}
+function verDeposito() {
+    const telaCreditar = document.getElementById("saida");
+    telaCreditar.classList.add("deposito");
+    telaCreditar.innerHTML = `Foi depositado o valor de R$  ${obterValor()}, com sucesso!`;
+   
+}
+function verExtrato() {
+    const telaExtrato = document.getElementById("saida");
+    telaExtrato.innerHTML = consultarExtrato();
+    const extratoFormatado = telaExtrato.classList.forEach('extrato');
+    return extratoFormatado;
+}
 function sacar(valor) {
     const sucesso = conta.debitar(valor);
     if (sucesso) {
-        alert(`Saque de R$ ${valor} efetuado com sucesso!`);
+        const telaSaque = document.getElementById("saida");
+        telaSaque.classList.add("saque");
+        telaSaque.innerHTML = `Saque de R$ ${valor} efetuado com sucesso!`;
+        
     } else {
-        alert(`Saldo insuficiente para saque de R$ ${valor}!`);
+        const ValorInsuficiente = document.getElementById("saida");
+        ValorInsuficiente.innerHTML = `Valor insuficiente!`;
+        ValorInsuficiente.classList.add("erro");
     }
+    
 }
 
 function consultarExtrato() {
@@ -93,7 +117,7 @@ function consultarExtrato() {
     for (let i = 0; i < extrato.length; i++) {
         formatado += extrato[i] + '\n';
     }
-    alert(formatado);
+    return formatado;
 }
 function obterValor() {
     const input = document.querySelector("#valor");
@@ -115,17 +139,20 @@ function limparSaida() {
 
 function validar(valor) {
     if (!valor || valor.length === 0) {
-        alert('Valor não preenchido!');
+        const ValorNaoPreenchido = document.getElementById("saida");
+        ValorNaoPreenchido.innerHTML = `Valor não preenchido!`;
         return false;
     }
     const regex = /^[0-9]*\.?[0-9]*$/;
     const isNumerico = valor.match(regex);
     if (!isNumerico) {
-        alert('Valor deve ser numérico, parte fracionária separada por ponto e ser maior que zero!');
+        const telaErro = document.getElementById("saida");
+        telaErro.innerHTML = `Valor deve ser numérico, parte fracionária separada por ponto e ser maior que zero!`;
         return false;
     }
     if (parseFloat(valor) === 0) {
-        alert('Valor não pode ser zero!');
+        const valorZero = document.getElementById("saida");
+        valorZero.innerHTML = `Valor não pode ser zero!`;
         return false;
     }
     return true;
